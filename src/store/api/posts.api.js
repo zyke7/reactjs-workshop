8880b77deb324dbeef2b62865e1a9f92e2fdbcfe
Dan <danphilip12@gmail.com>
2023-05-05 11:17:16 +0530

    Fixed relative json paths

--- src/store/api/posts.api.js
+++ src/store/api/posts.api.js
@@ -3,7 +3,7 @@ import commonApi from "./common.api";
 const postsApi = commonApi.injectEndpoints({
   endpoints: (build) => ({
     fetchPosts: build.query({
-      query: (pageNumber) => `/mock/CONTENTLISTINGPAGE-PAGE${pageNumber}.json`,
+      query: (pageNumber) => `mock/CONTENTLISTINGPAGE-PAGE${pageNumber}.json`,
 
     })
   })
